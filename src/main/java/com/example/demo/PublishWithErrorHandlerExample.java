86ace6b6032512f23b16bd2bef7efce65d349fdb
ninosantiago <nino.lucio.santiago@gmail.com>
2022-10-21 00:58:55 +1100

    working pubsub publish

---
+++ src/main/java/com/example/demo/PublishWithErrorHandlerExample.java
@@ -0,0 +1,80 @@
+package com.example.demo;
+
+import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutureCallback;
+import com.google.api.core.ApiFutures;
+import com.google.api.gax.rpc.ApiException;
+import com.google.cloud.pubsub.v1.Publisher;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.protobuf.ByteString;
+import com.google.pubsub.v1.PubsubMessage;
+import com.google.pubsub.v1.TopicName;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/*
+more info at: https://cloud.google.com/pubsub/docs/publisher
+ */
+public class PublishWithErrorHandlerExample {
+
+    public static void main(String... args) throws Exception {
+        // TODO(developer): Replace these variables before running the sample.
+        String projectId = "macro-bonito-168915";
+        String topicId = "demo-topic";
+
+        publishWithErrorHandlerExample(projectId, topicId);
+    }
+
+    public static void publishWithErrorHandlerExample(String projectId, String topicId)
+            throws IOException, InterruptedException {
+        TopicName topicName = TopicName.of(projectId, topicId);
+        Publisher publisher = null;
+
+        try {
+            // Create a publisher instance with default settings bound to the topic
+            publisher = Publisher.newBuilder(topicName).build();
+
+            List<String> messages = Arrays.asList("first message", "second message");
+
+            for (final String message : messages) {
+                ByteString data = ByteString.copyFromUtf8(message);
+                PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
+
+                // Once published, returns a server-assigned message id (unique within the topic)
+                ApiFuture<String> future = publisher.publish(pubsubMessage);
+
+                // Add an asynchronous callback to handle success / failure
+                ApiFutures.addCallback(
+                        future,
+                        new ApiFutureCallback<String>() {
+
+                            @Override
+                            public void onFailure(Throwable throwable) {
+                                if (throwable instanceof ApiException) {
+                                    ApiException apiException = ((ApiException) throwable);
+                                    // details on the API exception
+                                    System.out.println(apiException.getStatusCode().getCode());
+                                    System.out.println(apiException.isRetryable());
+                                }
+                                System.out.println("Error publishing message : " + message);
+                            }
+
+                            @Override
+                            public void onSuccess(String messageId) {
+                                // Once published, returns server-assigned message ids (unique within the topic)
+                                System.out.println("Published message ID: " + messageId);
+                            }
+                        },
+                        MoreExecutors.directExecutor());
+            }
+        } finally {
+            if (publisher != null) {
+                // When finished with the publisher, shutdown to free up resources.
+                publisher.shutdown();
+                publisher.awaitTermination(1, TimeUnit.MINUTES);
+            }
+        }
+    }
+}
